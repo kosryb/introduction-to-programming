4d0434dc8493e82323fe61c0ed16501361a154b4
Konstantin <90762262+kosryb@users.noreply.github.com>
2021-10-06 13:21:10 +0300

    Update 16_1.cpp

--- 16/16_1.cpp
+++ 16/16_1.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <locale.h>
 using namespace std;
 
 int main() {
+    setlocale(LC_ALL, "Rus");
     int n;
+    cout << "Введите число n:";
     cin >> n;
     vector <int> w(n);
     w[0] = 1;
+    cout << "Массив от 1 до n по нечетным индексам:" << endl;
     cout << w[0] << " ";
     for (int i = 1; i < n; i++) {
         w[i] = w[i - 1] + 2;
