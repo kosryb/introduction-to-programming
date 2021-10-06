c6ef9f41324f290098ffe3356095eff7d17332d4
Konstantin <90762262+kosryb@users.noreply.github.com>
2021-10-06 13:25:12 +0300

    Update 16_3.cpp

--- 16/16_3.cpp
+++ 16/16_3.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <locale.h>
 using namespace std;
 
 int main() {
+    setlocale(LC_ALL, "Rus");
     int n, a, b;
+    cout << "Введите целые числа N, A, B: ";
     cin >> n >> a >> b;
     vector <int> w(n);
     w[0] = a;
@@ -12,6 +15,7 @@ int main() {
     for (int i = 3; i < n; i++) {
         w[i] = w[i - 1] * 2;
     }
+    cout << "Целочисленный массив размера N, первый элемент которого равен A, второй равен B, а каждый последующий элемент равен сумме всех предыдущих" << endl;
     for (int i = 0; i < n; i++) {
         cout << w[i] << " ";
     }
