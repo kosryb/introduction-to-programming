fda70f959c19446fad910af17dbcfd88e46bcc50
Konstantin <90762262+kosryb@users.noreply.github.com>
2021-10-06 13:27:16 +0300

    Update 16_4.cpp

--- 16/16_4.cpp
+++ 16/16_4.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <locale.h>
 using namespace std;
 
 int main() {
+    setlocale(LC_ALL, "Rus");
     int n, l, r;
+    cout << "Введите целое число N (размер массива): ";
     cin >> n;
     vector <int> w(n);
     for (int i = 0; i < n; i++)
         cin >> w[i];
     l = 0;
     r = n - 1;
+    cout << "Элементы массива в следующем порядке: A1, AN , A2, AN−1, A3, AN−2" << endl;
     while (l <= r) {
         cout << w[l] << " ";
         if (l != r)
