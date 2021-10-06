a49bdf50a5468ce9eb969306e70904009954ea52
Konstantin <90762262+kosryb@users.noreply.github.com>
2021-10-06 13:23:21 +0300

    Update 16_2.cpp

--- 16/16_2.cpp
+++ 16/16_2.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <locale.h>
 using namespace std;
 
 int main() {
+    setlocale(LC_ALL, "Rus");
     int n, a, d;
+    cout << "Введите число N, первый член геометрической прогрессии и знаменатель:";
     cin >> n >> a >> d;
     vector <int> w(n);
     w[0] = a;
+    cout << "Массив, содержащий N первых членов геометрической прогрессии:" << endl;
     cout << w[0] << " ";
     for (int i = 1; i < n; i++) {
         w[i] = w[i - 1] * d;
