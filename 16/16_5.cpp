3c36aa3c6b937b43b4091b7396d7a9eda4ef86ba
Konstantin <90762262+kosryb@users.noreply.github.com>
2021-10-06 13:30:03 +0300

    Update 16_5.cpp

--- 16/16_5.cpp
+++ 16/16_5.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <locale.h>
 using namespace std;
 
 int main() {
+    setlocale(LC_ALL, "Rus");
     int n;
+    cout << "Введите целое число N (размер массива): ";
     cin >> n;
     vector <int> w(n);
+    cout << "Введите элементы массива: " << endl;
     for (int i = 0; i < n; i++)
         cin >> w[i];
+    cout << "Элементы массива с нечетными номерами в порядке возрастания номеров, а затем — элементы с четными номерами в порядке убывания номеров" << endl;
     for (int i = 0; i < n; i += 2)
         cout << w[i] << " ";
     n = (n % 2 == 0)? n: n - 1;
