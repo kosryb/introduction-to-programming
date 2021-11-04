c0a4a3131100145aec8127cd87cd506312eb1287
Konstantin <90762262+kosryb@users.noreply.github.com>
2021-11-04 17:21:56 +0300

    Update and rename 8_2.cpp to 13_2.cpp

---
+++ 13/13_2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <locale>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int n;
+    double s = 1;
+    cout << "Введите целое число N: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        s = s * (1 + 0.1 * i);
+    }
+    cout << "произведение 1.1 · 1.2 · 1.3 · . . . (N сомножителей) = " << s;
+}
