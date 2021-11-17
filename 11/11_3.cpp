7235655e322202de32076e99d8239fb3dd88eb03
Konstantin <90762262+kosryb@users.noreply.github.com>
2021-11-17 13:29:55 +0300

    Update 11_3.cpp

--- 11/11_3.cpp
+++ 11/11_3.cpp
@@ -6,11 +6,11 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    int a, b, c;
-    cout << "Введите числа a, b, c: ";
-    cin >> a >> b >> c;
-    if (abs(b - a) < abs(c - a)) 
-        cout << "расположена точка B ближе к А, расстояние АВ = " << abs(a - b);
+    int a1, a2, b1, b2, c1, c2;
+    cout << "Введите координаты a, b, c: ";
+    cin >> a1 >> a2 >> b1 >> b2 >> c1 >> c2;
+    if (sqrt(pow(a1 - b1, 2) + pow(a2 - b2, 2)) < sqrt(pow(a1 - c1, 2) + pow(a2 - c2, 2))) 
+        cout << "расположена точка B ближе к А, расстояние АВ = " << sqrt(pow(a1 - b1, 2) + pow(a2 - b2, 2));
     else
-        cout << "расположена точка C ближе к А, расстояние АC = " << abs(a - c);
+        cout << "расположена точка C ближе к А, расстояние АC = " << sqrt(pow(a1 - c1, 2) + pow(a2 - c2, 2));
 }
